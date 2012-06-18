a46cab86f0ffde6695300e56ea7a4d259b7b59a7
Charlie Somerville <charlie@charliesomerville.com>
2012-06-18 16:52:11 +1000

    command line args, sigint forwarding, EINTR handling

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 INSTALL_DIR=/usr/local/bin
-CFLAGS=-Wall -Werror -Wextra -pedantic -std=c99
+CFLAGS=-Wall -Werror -Wextra -pedantic -std=c99 -O3
 
 ptydo:
+
+clean:
+	rm -f ptydo
 
 install: ptydo
 	cp $^ $(INSTALL_DIR)
--- ptydo.c
+++ ptydo.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 #include <util.h>
+#include <errno.h>
 #include <signal.h>
 #include <sys/select.h>
+#include <sys/ioctl.h>
 
-/*
-void usage()
+static void usage()
 {
-    printf("Usage: ptydo [<switches>] [--] <command>\n");
-    printf("  -r    rows\n");
-    printf("  -c    columns\n");
+    printf(
+        "Usage: ptydo [<switches>] [--] <command...>\n"
+        "\n"
+        "Runs command in a child PTY with input/output exposed as stdin/stdout.\n"
+        "\n"
+        "Window Options:\n"
+        "  -w <columns>  Specifies the width of the PTY in columns\n"
+        "  -h <rows>     Specifies the height of the PTY in rows\n"
+        "\n"
+        "  If ptydo is run interactively, the PTY will default to the dimensions of the\n"
+        "  host PTY. Otherwise, the dimensions will default to 80 by 25.\n"
+        "\n"
+        "Misc Options:\n"
+        "  -c            Forward ^C to PTY. By default ^C kills ptydo.\n"
+    );
+    exit(EXIT_SUCCESS);
 }
-*/
+
+static pid_t child;
+static int ptyfd;
+
+static void exit_and_kill_child(int status) {
+    if(child) {
+        kill(child, SIGTERM);
+    }
+    exit(status);
+}
+
+static void forward_signal_to_pty(int sig) {
+    kill(child, sig);
+}
 
 int main(int argc, char** argv)
 {
@@ -24,8 +52,57 @@ int main(int argc, char** argv)
     window.ws_row = 25;
     window.ws_col = 80;
     
-    int ptyfd;
-    pid_t child = forkpty(&ptyfd, NULL, NULL, &window);
+    if(isatty(0)) {
+        ioctl(0, TIOCGWINSZ, &window);
+    }
+    
+    if(argc < 2) {
+        usage();
+    }
+    
+    int argi;
+    for(argi = 1; argi < argc; argi++) {
+        if(argv[argi][0] != '-') {
+            break;
+        }
+        if(strlen(argv[argi]) > 2) {
+            printf("Unknown option '%s'\n", argv[argi]);
+            exit(EXIT_FAILURE);
+        }
+        if(argv[argi][1] == '-') {
+            argi++;
+            break;
+        }
+        switch(argv[argi][1]) {
+            case 'w':
+                if(argi + 1 == argc) {
+                    printf("Expected PTY width after -w flag");
+                    exit(EXIT_FAILURE);
+                }
+                window.ws_col = atoi(argv[++argi]);
+                break;
+            case 'h':
+                if(argi + 1 == argc) {
+                    printf("Expected PTY height after -h flag");
+                    exit(EXIT_FAILURE);
+                }
+                window.ws_row = atoi(argv[++argi]);
+                break;
+            case 'c':
+                signal(SIGINT, forward_signal_to_pty);
+                break;
+            default:
+                printf("Unknown option '-%c'\n", argv[argi][1]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    
+    if(argi == argc) {
+        printf("No command specified\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    child = forkpty(&ptyfd, NULL, NULL, &window);
     
     if(child == -1) {
         // forkpty failed:
@@ -37,8 +114,10 @@ int main(int argc, char** argv)
         // we are the child process
 //        execvp(argv[1], argv + 1);
         // execvp failed:
-        char* ptyargv[] = { "bash", NULL };
-        execv("/bin/bash", ptyargv);
+        char* ptyargv[argc - argi + 1];
+        memcpy(ptyargv, argv + argi, (argc - argi) * sizeof(char*));
+        ptyargv[argc - argi] = NULL;
+        execvp(ptyargv[0], ptyargv);
         perror("execvp");
         exit(EXIT_FAILURE);
     }
@@ -52,10 +131,15 @@ int main(int argc, char** argv)
         FD_ZERO(&errfds);
         FD_SET(0, &errfds);
         FD_SET(ptyfd, &errfds);
+        
         int err = select(ptyfd + 1, &readfds, NULL, &errfds, NULL);
         if(err < 0) {
+            if(errno == EINTR) {
+                continue;
+            }
             perror("select");
         }
+        
         int readfd, writefd;
         if(FD_ISSET(0, &readfds)) {
             // data on stdin
@@ -64,20 +148,20 @@ int main(int argc, char** argv)
         } else if(FD_ISSET(ptyfd, &readfds)) {
             writefd = 0;
             readfd = ptyfd;
-        } else if(FD_ISSET(0, &errfds) || FD_ISSET(0, &readfds)) {
-            kill(child, SIGTERM);
-            exit(EXIT_SUCCESS);
+        } else if(FD_ISSET(0, &errfds) || FD_ISSET(ptyfd, &readfds)) {
+            exit_and_kill_child(EXIT_SUCCESS);
         } else {
             fprintf(stderr, "this should never happen\n");
-            exit(EXIT_FAILURE);
+            exit_and_kill_child(EXIT_FAILURE);
         }
+        
         char buff[1024];
         ssize_t bytes = read(readfd, buff, sizeof(buff));
         if(bytes == 0) {
-            kill(child, SIGTERM);
-            exit(EXIT_SUCCESS);
+            exit_and_kill_child(EXIT_SUCCESS);
         } else if(bytes < 0) {
             perror("read");
+            exit_and_kill_child(EXIT_FAILURE);
         }
         write(writefd, buff, bytes);
     }
